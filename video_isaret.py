2056df662149bf2ee3d2ae4b336a4c4f9be88d4a
Serhat Obay <75859932+SerhatObay@users.noreply.github.com>
2023-12-21 13:08:53 +0300

    Website Added

--- video_isaret.py
+++ video_isaret.py
@@ -1,51 +1,51 @@
-import cv2 
-import pickle
+from flask import Flask, render_template, jsonify
+from flask_socketio import SocketIO
+import cv2
 import numpy as np
+import pickle
+
+app = Flask(__name__)
+socketio = SocketIO(app)
 
 cap = cv2.VideoCapture("video.mp4")
 
 def check(frame1):
-    space_counter=0
+    space_counter = 0
     for pos in liste:
-        x,y = pos
+        x, y = pos
 
-        crop=frame1[y:y+15,x:x+26]
-        count=cv2.countNonZero(crop)
-        #print("count")
-        
+        crop = frame1[y:y+15, x:x+26]
+        count = cv2.countNonZero(crop)
+
         if count <= 150:
-            color=(0,255,0)
-            space_counter+=1
-        else:
-            color=(0,0,255)
+            space_counter += 1
 
-        cv2.rectangle(frame,pos,(pos[0]+24,pos[1]+12),color,2)
-    
-    percentage = int(100-(space_counter / len(liste)) * 100)
+    percentage = int(100 - (space_counter / len(liste)) * 100)
 
-    cv2.putText(frame, f"Doluluk: %{percentage}", (12, 339), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 3, cv2.LINE_AA)
+    return percentage
 
-    
+@app.route('/')
+def index():
+    return render_template('index_ajax.html')
 
-with open("noktalar","rb") as f:
-    liste = pickle.load(f)
+@app.route('/get_percentage', methods=['GET'])
+def get_percentage():
+    _, frame = cap.read()
 
-while True:
-    _,frame = cap.read()
-    
-    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    blur=cv2.GaussianBlur(gray,(3,3),1)
-    thresh=cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV,25,16)
-    median=cv2.medianBlur(thresh,5)
-    dilates=cv2.dilate(median,np.ones((3,3)),iterations=1)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (3, 3), 1)
+    thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 25, 16)
+    median = cv2.medianBlur(thresh, 5)
+    dilates = cv2.dilate(median, np.ones((3, 3)), iterations=1)
 
+    percentage = check(dilates)
 
-    check(dilates)
-    cv2.imshow("video",frame)
-    
+    cv2.imwrite("static/video_frame.jpg", frame)  # Save the frame as a static image
 
-    if cv2.waitKey(200) & 0XFF == ord("q"):
-        break
+    return jsonify({'percentage': percentage, 'image_url': '/static/video_frame.jpg'})
 
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    with open("noktalar", "rb") as f:
+        liste = pickle.load(f)
+
+    socketio.run(app, debug=True)
